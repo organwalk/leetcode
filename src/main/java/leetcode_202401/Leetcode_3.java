4414c6b38f3df3f9e3d30734e3e192fc41d2af59
organwalk <hinzink@foxmail.com>
2024-01-01 16:52:23 +0800

    2024-01-01

---
+++ src/main/java/leetcode_202401/Leetcode_3.java
@@ -0,0 +1,51 @@
+package leetcode_202401;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ * ----------------------------------------------------
+ * 示例 1:
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+public class Leetcode_3 {
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int start = 0;
+        int end = 0;
+        int maxLength = 0;
+        while (end < s.length()) {
+            char currentChar = s.charAt(end);
+            if (set.contains(currentChar)) {
+                set.remove(s.charAt(start));
+                start++;
+            } else {
+                set.add(currentChar);
+                maxLength = Math.max(maxLength, end - start + 1);
+                end++;
+            }
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+
+}
