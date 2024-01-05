46d7bb97c06f13b899a06428b92bbc9c4539d101
organwalk <hinzink@foxmail.com>
2024-01-05 11:11:54 +0800

    2023-01-05

---
+++ src/main/java/leetcode_202401/Leetcode_5.java
@@ -0,0 +1,51 @@
+package leetcode_202401;
+
+import java.util.Objects;
+
+/**
+ * 5.最长回文子串
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ * 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+ * -----------------------------------------------
+ * 示例 1：
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * 示例 2：
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ */
+public class Leetcode_5 {
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            int len1 = helper(s, i, i);
+            int len2 = helper(s, i, i+1);
+
+            int maxLen = Math.max(len1, len2);
+
+            if (maxLen > end - start){
+                start = i - (maxLen - 1) / 2;
+                end = i + maxLen / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int helper(String s, int left, int right){
+        while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babad"));
+    }
+}
