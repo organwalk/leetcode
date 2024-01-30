fc79b8749dd46e40cd80c5d586960ab6318f9079
organwalk <hinzink@foxmail.com>
2024-01-30 22:05:57 +0800

    2023-01-30

---
+++ src/main/java/leetcode_202401/Leetcode_6.java
@@ -0,0 +1,65 @@
+package leetcode_202401;
+
+/**
+ * 6. Z字形变换
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ * string convert(string s, int numRows);
+ * ---------------------------------------
+ * 示例 1：
+ * 输入：s = "PAYPALISHIRING", numRows = 3
+ * 输出："PAHNAPLSIIGYIR"
+ * 示例 2：
+ * 输入：s = "PAYPALISHIRING", numRows = 4
+ * 输出："PINALSIGYAHRPI"
+ * 解释：
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ * 示例 3：
+ * 输入：s = "A", numRows = 1
+ * 输出："A"
+ */
+public class Leetcode_6 {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows[curRow].append(c);
+
+            if (curRow == 0 || curRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Leetcode_6 solution = new Leetcode_6();
+        System.out.println(solution.convert("PAYPALISHIRING", 3));
+    }
+}
